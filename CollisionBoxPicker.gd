6ef1f610e75d94de3686103a373daed09a24895f
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2023-12-17 16:27:19 -0300

    Removed unused assets

--- CollisionBoxPicker.gd
+++
@@ -1,4 +0,0 @@
-extends OptionButton
-
-func _on_item_selected(_index: int) -> void:
-    release_focus()
--- world.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	$CharacterBody3D.do_stairs = $StairsSetting.button_pressed
-	$CharacterBody3D.do_skipping_hack = $SkippingSetting.button_pressed
