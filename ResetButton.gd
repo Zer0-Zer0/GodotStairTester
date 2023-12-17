eaf96368aa1317ff81bb6516063b5ec355afb853
Zer0-Zer0 <100931027+Zer0-Zer0@users.noreply.github.com>
2023-12-17 13:58:54 -0300

    Made the players it's own separate thing

--- ResetButton.gd
+++ ResetButton.gd
@@ -2,11 +2,11 @@ extends Button
 
 var start_pos = Vector3()
 func _ready() -> void:
-    start_pos = $"../CharacterBody3D".global_position
+	start_pos = $"../Player".global_position
 
 func _on_pressed() -> void:
-    $"../CharacterBody3D".global_position = start_pos
-    $"../CharacterBody3D".velocity *= 0.0
-    $"../CharacterBody3D/CameraHolder".rotation.y = 0.0
-    $"../CharacterBody3D/CameraHolder".rotation.x = 0.0
-    release_focus()
+	$"../Player".global_position = start_pos
+	$"../Player".velocity *= 0.0
+	$"../Player/CameraHolder".rotation.y = 0.0
+	$"../Player/CameraHolder".rotation.x = 0.0
+	release_focus()
